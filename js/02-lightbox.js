2ec430f75771ca722c69a828766a495119c01ed8
GrzegorzKobuszewski <kobuszewskigrzegorz@gmail.com>
2022-07-24 23:46:23 +0200

    Zadanie 2
    Utworzenie galerii z pomocą biblioteki SimpleLightbox, która umożliwia płynne przejścia pomiędzy wyświetlanymi zdjęciami, również za pomocą klawiatury.
    
    Ma dużo różnych ustawień, dokumentacja biblioteki:
    https://simplelightbox.com/

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,24 @@
-import { galleryItems } from './gallery-items.js';
-// Change code below this line
+import { galleryItems } from "./gallery-items.js";
 
-console.log(galleryItems);
+const gallery = document.querySelector(".gallery");
+const newGalleryItems = [];
+
+galleryItems.forEach(e => {
+    const galleryItemLink = document.createElement("a");
+    const galleryItemImage = document.createElement("img");
+    
+    galleryItemLink.className = "gallery__link";
+    galleryItemImage.className = "gallery__image";
+
+    galleryItemLink.href = e.original;
+    galleryItemImage.src = e.preview;
+    galleryItemImage.setAttribute("title", e.description);
+    galleryItemImage.alt = e.description;
+    
+    galleryItemLink.append(galleryItemImage);
+    newGalleryItems.push(galleryItemLink);
+});
+
+gallery.append(...newGalleryItems);
+
+new SimpleLightbox(".gallery a", { captionDelay: 250 });
