513c3de2093093548488bef1dc0147eb746c4a55
GrzegorzKobuszewski <kobuszewskigrzegorz@gmail.com>
2022-07-24 23:03:34 +0200

    Zadanie 1
    Galeria zdjęć zrobiona z możliwością kliknięcia w każdy element i przeglądania pełnego obrazu w oknie modalnym.
    
    Wykorzystane funkcje/metody/biblioteki
    
    basicLightbox - const instance = basicLightbox.create, instance.show();
    
    e.preventDefault() - w celu skasowania domyślnego działania przeglądarki np. przy klikaniu w link (będący zdjęciem) przeniesie na stronę ze zdjęciem zgodnie z linkiem, preventDefault () zapobiegnie temu...

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,22 +1,20 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 
-console.log(galleryItems)
-
-const gallery = document.querySelector('.gallery');
+const gallery = document.querySelector(".gallery");
 const newGalleryItems = [];
 
 galleryItems.forEach(e => {
-    const galleryItem = document.createElement('div');
-    const galleryItemLink = document.createElement('a');
-    const galleryItemImage = document.createElement('img');
+    const galleryItem = document.createElement("div");
+    const galleryItemLink = document.createElement("a");
+    const galleryItemImage = document.createElement("img");
     
-    galleryItem.className = 'gallery__item';
-    galleryItemLink.className = 'gallery__link';
-    galleryItemImage.className = 'gallery__img';
+    galleryItem.className = "gallery__item";
+    galleryItemLink.className = "gallery__link";
+    galleryItemImage.className = "gallery__image";
 
     galleryItemLink.href = e.original;
-    galleryItemImage.scr = e.preview;
-    galleryItemImage.setAttribute('data-source', e.original);
+    galleryItemImage.src = e.preview;
+    galleryItemImage.setAttribute("data-source", e.original);
     galleryItemImage.alt = e.description;
     
     galleryItem.append(galleryItemLink);
@@ -26,10 +24,21 @@ galleryItems.forEach(e => {
 
 gallery.append(...newGalleryItems);
 
+gallery.addEventListener("click", e => {
+    e.preventDefault();
+    if (e.target.nodeName !== "IMG") return;
+    const imageDataSource = e.target.getAttribute("data-source");
 
+    const instance = basicLightbox.create(
+        `<img src="${imageDataSource}" width="800" height="600">`,
+        {
+            onShow: (instance) => {
+                document.addEventListener("keydown", (e) => {
+                    if (e.key === "Escape") instance.close();
+                });
+            }
+        });
+    
+    instance.show();
 
-
-
-
-
-
+});
