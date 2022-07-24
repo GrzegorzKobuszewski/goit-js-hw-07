a5b2f898a0372b21fea3b3f373bf904e44c14841
GrzegorzKobuszewski <kobuszewskigrzegorz@gmail.com>
2022-07-24 18:47:22 +0200

    Zadanie 1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,6 +1,35 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
 
-const list = document.querySelector("");
+console.log(galleryItems)
 
-console.log(galleryItems);
+const gallery = document.querySelector('.gallery');
+const newGalleryItems = [];
+
+galleryItems.forEach(e => {
+    const galleryItem = document.createElement('div');
+    const galleryItemLink = document.createElement('a');
+    const galleryItemImage = document.createElement('img');
+    
+    galleryItem.className = 'gallery__item';
+    galleryItemLink.className = 'gallery__link';
+    galleryItemImage.className = 'gallery__img';
+
+    galleryItemLink.href = e.original;
+    galleryItemImage.scr = e.preview;
+    galleryItemImage.setAttribute('data-source', e.original);
+    galleryItemImage.alt = e.description;
+    
+    galleryItem.append(galleryItemLink);
+    galleryItemLink.append(galleryItemImage);
+    newGalleryItems.push(galleryItem);
+});
+
+gallery.append(...newGalleryItems);
+
+
+
+
+
+
+
+
